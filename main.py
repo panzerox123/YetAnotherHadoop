587214f32b263c9aefc554df682283d7f9482347
Kunal Bhat <kunal.bhat2001@gmail.com>
2021-11-08 20:45:11 +0530

    Reading configuration and creating files

--- main.py
+++ main.py
@@ -1,10 +1,75 @@
 import json
+from json.decoder import JSONDecodeError
 import sys
+import os
+
+def create_dfs():
+    config_path = input("Enter path for configuration file: ")
+    try:
+        config_file = open(os.path.expandvars(config_path), 'r')
+        config = json.load(config_file)
+        config_file.close()
+    except FileNotFoundError:
+        print("Path does not exist")
+        exit(-1)
+    except JSONDecodeError:
+        print("Configuration file not formatted correctly")
+        exit(-1)
+    try:
+        os.makedirs(os.path.expandvars(config['path_to_datanodes']), exist_ok=True)
+        os.makedirs(os.path.expandvars(config['path_to_namenodes']), exist_ok=True)
+        os.makedirs(os.path.expandvars(config['datanode_log_path']), exist_ok=True)
+        os.makedirs(os.path.expandvars(os.path.dirname(config['namenode_log_path'])), exist_ok=True)
+        os.makedirs(os.path.expandvars(config['namenode_checkpoints']), exist_ok=True)
+        os.makedirs(os.path.expandvars(config['fs_path']), exist_ok=True)
+        os.makedirs(os.path.expandvars(os.path.dirname(config['dfs_setup_config'])), exist_ok=True)
+        setup_config_path = os.path.expandvars(config['dfs_setup_config'])
+        setup_config_file = open(setup_config_path , 'w')
+        json.dump(config, setup_config_file)
+        setup_config_file.close()
+        cache_file = open('./cache_file', 'w')
+        print(setup_config_path, file=cache_file)
+        cache_file.close()
+    except Exception as e:
+        print("Error encountered in configuration file:", e)
+        exit(-1)
+    exit(0)
+
+def cli():
+    pass
 
 def __main__():
-    if sys.argv[1] == "create":
-        config_file = open(sys.argv[2])
-        configuration = json.load(config_file)
-        print(configuration) 
+    try:
+        dfs_setup_config_path = os.path.expandvars(sys.argv[1], 'r')
+        dfs_setup_config_file = open(dfs_setup_config_path, 'r')
+        dfs_setup_config = json.load(dfs_setup_config_file)
+        dfs_setup_config_file.close()
+        cache_file = open('./cache_file', 'w')
+        print(sys.argv[1], file=cache_file)
+    except IndexError:
+        try:
+            cache_file = open('./cache_file', 'r')
+            dfs_setup_config_path = os.path.expandvars(cache_file.readline().strip())
+            dfs_setup_config_file = open(dfs_setup_config_path, 'r')
+            dfs_setup_config = json.load(dfs_setup_config_file)
+            dfs_setup_config_file.close()
+            cache_file.close()
+        except:
+            r = input('Would you like to create a new DFS? [y/n]: ')
+            if r.lower() == 'y':
+                create_dfs()
+            else:
+                exit(0)
+    except FileNotFoundError:
+        r = input('The specified DFS does not exist. Would you like to create a new one? [y/n]: ')
+        if r.lower() == 'y':
+            create_dfs()
+        else:
+            exit(0)
+    except Exception as e:
+        print("Unhandled exception occurred:", e)
+        exit(-1)
+    print(dfs_setup_config)
+    exit(0)
 
 __main__()
