8564519e1b28f379c1b70480696829c62a791112
utkarsh348 <utkarsh348@gmail.com>
2021-11-13 11:49:18 +0530

    in progress

--- datanode.py
+++ datanode.py
@@ -1,6 +1,13 @@
-from io import SEEK_SET, FileIO
 import os
 
-def writer(datanode, block, data):
+def updateLog(log, datanode, block, action):
+
+    
+    pass
+
+def writer(datanode, block, data, log):
     with open(os.path.join(datanode, block), 'w') as f:
                 f.write(data)
+                f.close()
+    updateLog(log, datanode, block, 1)
+
