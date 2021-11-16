905e5fcb9cecfaacaec14f42daa59f4ca0648635
utkarsh348 <utkarsh348@gmail.com>
2021-11-16 22:09:11 +0530

    datanode server and write

--- datanode.py
+++ datanode.py
@@ -1,7 +1,9 @@
 import os
 import fileinput
+import socket
+import tqdm
 
-def updateLog(log, datanode, block, action):
+'''def updateLog(log, datanode, block, action):
     log = log + "/datanode"+datanode[-1]
     with open(log, "r") as f:
         contents = f.readlines()
@@ -26,3 +28,58 @@ def writer(datanode, block, data, log):
 def createNodes(path, num):
     for i in range(num):
         os.makedirs(os.path.expandvars(path+"/datanode"+str(i+1)))
+'''
+
+SERVER_HOST = "0.0.0.0"
+
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
+
+
+class Datanode:
+    def __init__(self, path, size, blocks, logPath, num, port):
+        
+        self.path = path
+        self.size = size
+        self.blocks = blocks
+        self.logPath = logPath
+        self.num = num
+        self.SERVER_PORT = port
+        s = socket.socket()
+
+        s.bind((SERVER_HOST, self.SERVER_PORT))
+        print(f"[*] Listening as {SERVER_HOST}:{self.SERVER_PORT}")
+        
+        client_socket, address = s.accept() 
+        self.server(client_socket)
+        pass
+
+    # def recieve files
+    # def write files
+    # def update datanode log
+    # def return status to namenode
+
+    def server(client_socket):
+        
+        received = client_socket.recv(BUFFER_SIZE).decode()
+        filename, filesize = received.split(SEPARATOR)
+        # remove absolute path if there is
+        filename = os.path.basename(filename)
+        # convert to integer
+        filesize = int(filesize)
+        #filename can be the block number specified by the namenode since its keeping track anyway
+        progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        while True:
+            with open(filename, "wb") as f:
+                while True:
+                    bytes_read = client_socket.recv(BUFFER_SIZE)
+                    if not bytes_read:   
+                        break
+                    f.write(bytes_read)
+                    progress.update(len(bytes_read))
+
+            # close the client socket
+        client_socket.close()
+        # close the server socket
+        s.close()
+        pass
